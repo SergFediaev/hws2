192a4e1689deaba7ad68ab093e54a4d2f0de6861
SergFediaev <sergfediaev@gmail.com>
2024-04-22 17:34:58 +0300

    HW8.

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const stateCopy = state.map(user => ({...user}))
+            if (action.payload === 'up') stateCopy.sort((userA, userB) => userA.name > userB.name ? 1 : -1)
+            else if (action.payload === 'down') stateCopy.sort((userA, userB) => userA.name < userB.name ? 1 : -1)
+            return stateCopy // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= 18) // need to fix
         }
         default:
             return state
